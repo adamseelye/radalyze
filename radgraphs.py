29085255e0e4b2bee3ca1bc9fde049331eb435fa
adamseelye <adam945@revature.net>
2022-06-21 22:22:00 -0600

    improved graph menu functionality

--- radgraphs.py
+++ radgraphs.py
@@ -4,12 +4,66 @@ import pandas as pd
 class graph:
     def radiation_graphs():
         try:
+            state = input("Please choose radiation data from Ari(Z)ona, (L)ouisiana, Ma(S)sachussetts, or (M)ontana: ")
 
-            df = pd.read_csv("testdata/AL_BIRMINGHAM_2020.csv")
+            if state == 'z' or state == 'Z':
+                year = input("Please choose the year to analyze and enter the last two digits, from between 08 and 21: ")
+                print("\n")
+                print(f"~~~Displaying radiation data from the Gamma detector in Tucson, Arizona for the year 20{year}~~~")
+                print("")
+                print("The X axis displays the Gamma detection count in Counts Per Minute (CPM)")
+                print("The Y axis displays the data point count collected over the entire year, with January at the origin")
+                print("\n")
 
-            df = df.groupby(['SAMPLE COLLECTION TIME', 'GAMMA COUNT RATE R02 (CPM)', 'GAMMA COUNT RATE R05 (CPM)', 'GAMMA COUNT RATE R09 (CPM)'])[['DOSE EQUIVALENT RATE (nSv/h)']].mean()
+                df = pd.read_csv(f"raddata/az_tucson/AZ_TUCSON_20{year}.csv")
+                df = df.groupby(['SAMPLE COLLECTION TIME', 'GAMMA COUNT RATE R02 (CPM)', 'GAMMA COUNT RATE R05 (CPM)', 'GAMMA COUNT RATE R09 (CPM)'])[['DOSE EQUIVALENT RATE (nSv/h)']].mean()
 
-            df.reset_index(inplace=True)
+                df.reset_index(inplace=True)
+
+            elif state == 'l' or state == 'L':
+                year = input("Please choose the year to analyze and enter the last two digits, from between 08 and 21: ")
+                print("\n")
+                print(f"~~~Displaying radiation data from the Gamma detector in Baton Rouge, Louisiana for the year 20{year}~~~")
+                print("")
+                print("The X axis displays the Gamma detection count in Counts Per Minute (CPM)")
+                print("The Y axis displays the data point count collected over the entire year, with January at the origin")
+                print("\n")
+
+                df = pd.read_csv(f"raddata/la_baton_rouge/LA_BATON_ROUGE_20{year}.csv")
+                df = df.groupby(['SAMPLE COLLECTION TIME', 'GAMMA COUNT RATE R02 (CPM)', 'GAMMA COUNT RATE R05 (CPM)', 'GAMMA COUNT RATE R09 (CPM)'])[['DOSE EQUIVALENT RATE (nSv/h)']].mean()
+
+                df.reset_index(inplace=True)
+
+            elif state == 's' or state == 'S':
+                year = input("Please choose the year to analyze and enter the last two digits, from between 06 and 21: ")
+                print("\n")
+                print(f"~~~Displaying radiation data from the Gamma detector in Boston, Massachussetts for the year 20{year}~~~")
+                print("")
+                print("The X axis displays the Gamma detection count in Counts Per Minute (CPM)")
+                print("The Y axis displays the data point count collected over the entire year, with January at the origin")
+                print("\n")
+
+                df = pd.read_csv(f"raddata/ma_boston/MA_BOSTON_20{year}.csv")
+                df = df.groupby(['SAMPLE COLLECTION TIME', 'GAMMA COUNT RATE R02 (CPM)', 'GAMMA COUNT RATE R05 (CPM)', 'GAMMA COUNT RATE R09 (CPM)'])[['DOSE EQUIVALENT RATE (nSv/h)']].mean()
+
+                df.reset_index(inplace=True)
+
+            elif state == 'm' or state == 'M':
+                year = input("Please choose the year to analyze and enter the last two digits, from between 17 and 21: ")
+                print("\n")
+                print(f"~~~Displaying radiation data from the Gamma detector in Kalispell, Montana for the year 20{year}~~~")
+                print("")
+                print("The X axis displays the Gamma detection count in Counts Per Minute (CPM)")
+                print("The Y axis displays the data point count collected over the entire year, with January at the origin")
+                print("\n")
+
+                df = pd.read_csv(f"raddata/mt_kalispell/MT_KALISPELL_20{year}.csv")
+                df = df.groupby(['SAMPLE COLLECTION TIME', 'GAMMA COUNT RATE R02 (CPM)', 'GAMMA COUNT RATE R05 (CPM)', 'GAMMA COUNT RATE R09 (CPM)'])[['DOSE EQUIVALENT RATE (nSv/h)']].mean()
+
+                df.reset_index(inplace=True)
+            
+            else:
+                exit()
 
         except:
             print("Data Error")
